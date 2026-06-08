d8157dfe2ea7a8cbea0448422189b131b08062d5
InvisibleSpiders <132212539+InvisibleSpiders@users.noreply.github.com>
2026-06-08 12:15:24 -0600

    Test VaultUnlocked plugin availability separately

--- haven-core/src/test/java/dev/invisiblespiders/haven/core/hook/VaultUnlockedHookTest.java
+++ haven-core/src/test/java/dev/invisiblespiders/haven/core/hook/VaultUnlockedHookTest.java
@@ -2,6 +2,8 @@ package dev.invisiblespiders.haven.core.hook;
 
 import net.milkbowl.vault2.economy.Economy;
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.ServicesManager;
 import org.junit.jupiter.api.Test;
@@ -19,11 +21,34 @@ class VaultUnlockedHookTest {
 
         try (MockedStatic<Bukkit> bukkit = mockStatic(Bukkit.class)) {
             bukkit.when(Bukkit::getServicesManager).thenReturn(services);
+            bukkit.when(Bukkit::getPluginManager).thenReturn(mock(PluginManager.class));
 
             VaultUnlockedHook hook = new VaultUnlockedHook();
             hook.onEnable();
 
             assertFalse(hook.isAvailable());
+            assertNull(hook.getEconomy());
+            verify(services).getRegistration(Economy.class);
+        }
+    }
+
+    @Test
+    void onEnableIsAvailableWhenPluginIsInstalledWithoutEconomyProvider() {
+        ServicesManager services = mock(ServicesManager.class);
+        when(services.getRegistration(Economy.class)).thenReturn(null);
+
+        PluginManager plugins = mock(PluginManager.class);
+        when(plugins.getPlugin("VaultUnlocked")).thenReturn(mock(Plugin.class));
+
+        try (MockedStatic<Bukkit> bukkit = mockStatic(Bukkit.class)) {
+            bukkit.when(Bukkit::getServicesManager).thenReturn(services);
+            bukkit.when(Bukkit::getPluginManager).thenReturn(plugins);
+
+            VaultUnlockedHook hook = new VaultUnlockedHook();
+            hook.onEnable();
+
+            assertTrue(hook.isAvailable());
+            assertFalse(hook.hasEconomyProvider());
             assertNull(hook.getEconomy());
             verify(services).getRegistration(Economy.class);
         }
@@ -40,11 +65,13 @@ class VaultUnlockedHookTest {
 
         try (MockedStatic<Bukkit> bukkit = mockStatic(Bukkit.class)) {
             bukkit.when(Bukkit::getServicesManager).thenReturn(services);
+            bukkit.when(Bukkit::getPluginManager).thenReturn(mock(PluginManager.class));
 
             VaultUnlockedHook hook = new VaultUnlockedHook();
             hook.onEnable();
 
             assertTrue(hook.isAvailable());
+            assertTrue(hook.hasEconomyProvider());
             assertSame(economy, hook.getEconomy());
             verify(services).getRegistration(Economy.class);
         }
@@ -61,12 +88,14 @@ class VaultUnlockedHookTest {
 
         try (MockedStatic<Bukkit> bukkit = mockStatic(Bukkit.class)) {
             bukkit.when(Bukkit::getServicesManager).thenReturn(services);
+            bukkit.when(Bukkit::getPluginManager).thenReturn(mock(PluginManager.class));
 
             VaultUnlockedHook hook = new VaultUnlockedHook();
             hook.onEnable();
             hook.onDisable();
 
             assertFalse(hook.isAvailable());
+            assertFalse(hook.hasEconomyProvider());
             assertNull(hook.getEconomy());
         }
     }
